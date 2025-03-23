e45049809416f97ab2a649d4c9c1547d9d46eead
riessd <riessd@spseplzen.cz>
2025-03-23 14:59:35 +0100

    pridany obrazky

--- app/(game)/imageMap.tsx
+++ app/(game)/imageMap.tsx
@@ -2,6 +2,10 @@ export const imagesMap: Record<string, any> = {
     "PES": require("../../assets/images/win_images/dog.png"),
     "KOČKA": require("../../assets/images/win_images/cat.png"),
     "LEV": require("../../assets/images/win_images/lion.png"),
-    "KŮŇ": require("../../assets/images/win_images/lion.png"),
+    "KŮŇ": require("../../assets/images/win_images/horse.png"),
+    "TYGR": require("../../assets/images/win_images/tiger.png"),
     "MAPA": require("../../assets/images/win_images/map.png"),
+    "VOLANT": require("../../assets/images/win_images/steering.png"),
+    "HVĚZDA": require("../../assets/images/win_images/star.png"),
+    "MĚSÍC": require("../../assets/images/win_images/moon.png"),
   };
